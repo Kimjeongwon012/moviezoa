2469346985240a2b0e0a3f35fbfed7093c57d638
Kimjeongwon <comkjo0512@gmail.com>
2025-02-28 18:29:17 +0900

    기본 프로필 사진 설정 완료

--- src/main/java/me/jeong/movieinfo/controller/ActorController.java
+++ src/main/java/me/jeong/movieinfo/controller/ActorController.java
@@ -3,6 +3,10 @@ package me.jeong.movieinfo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +14,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 @RestController
 @RequestMapping("/api/actors")
 public class ActorController {
     private static final Logger log = LoggerFactory.getLogger(MovieController.class);
 
     @GetMapping("/default-profile/{gender}")
-    public String getDefaultProfileImage(@PathVariable("gender") int gender) {
-        log.info("1ss");
+    public ResponseEntity<byte[]> getDefaultProfileImage(@PathVariable("gender") int gender) {
+        String url = "";
         if (gender == 2) {
-            return "/img/default-profileM.png";
-        } else if (gender == 1) {
-            return "/img/default-profileW.png";
+            url = "/img/default-profileM.jpg";
         } else {
-            return "/img/default-profileM.png";
+            url = "/img/default-profileW.jpg";
         }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "image/jpg");
+        try {
+            Resource resource = new ClassPathResource("static" + url);
+            byte[] imageBytes = Files.readAllBytes(resource.getFile().toPath());
+            log.info(resource.getURL().toString());
+
+            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+        } catch (IOException e) {
+            log.info(e.getMessage());
+        }
+
+        return new ResponseEntity<>(null, headers, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
